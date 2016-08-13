fdcc19be129e08e67eb338b5535575b87efd7a7c
kiang <kiange@gmail.com>
2016-08-13 12:45:35 +0800

    move raw path

--- raw.php
+++ raw.php
@@ -1,7 +1,7 @@
 <?php
 
 $fh = fopen(__DIR__ . '/list.csv', 'r');
-$rawPath = __DIR__ . '/raw/' . date('Ymd');
+$rawPath = __DIR__ . '/raw';
 if (!file_exists($rawPath)) {
     mkdir($rawPath, 0777, true);
 }
@@ -9,9 +9,7 @@ $header = '';
 while ($line = fgetcsv($fh, 2048)) {
     if (!empty($line[1])) {
         $line[1] = trim($line[1]);
-        if (!file_exists($rawPath . '/' . $line[1])) {
-            file_put_contents($rawPath . '/' . $line[1], file_get_contents('http://opendata.cwb.gov.tw/opendataapi?dataid=' . $line[1] . '&authorizationkey=CWB-15296E81-2B15-4277-BE49-549BCEAFD553'));
-        }
+        file_put_contents($rawPath . '/' . $line[1], file_get_contents('http://opendata.cwb.gov.tw/opendataapi?dataid=' . $line[1] . '&authorizationkey=CWB-15296E81-2B15-4277-BE49-549BCEAFD553'));
         $size = filesize($rawPath . '/' . $line[1]) / 1024 / 1024;
         if ($size > 100) {
             $zip = new ZipArchive();
